932af913bba87372ffaf4bf741c50936916752a7
pluton <amirrpp@yandex.ru>
2015-11-05 14:50:15 +0200

    create models
    - question model
    - answer model

--- answer/models.py
+++ answer/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    user_from = models.ForeignKey(User, related_name='questions_from')
+    user_to = models.ForeignKey(User, related_name='questions_to')
+
+    question = models.TextField(max_length=300)
+
+    date_question = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey('answer.Question', related_name='answers')
+
+    date_answer = models.DateTimeField(auto_now_add=True)
+
